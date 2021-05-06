3da13f3eb8a8d635b41ffc29045f48aa4f5e5c69
iulian <iulian.rusu999@gmail.com>
2021-05-06 14:28:51 +0300

    Add files

---
+++ server/led.py
@@ -0,0 +1,22 @@
+import RPi.GPIO as GPIO
+import socket
+from camera import LED_PORT
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(37,GPIO.OUT)
+GPIO.output(37,1)
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    sock.connect(('localhost', LED_PORT))
+    while True:
+        try:
+            sent = sock.send(b'0')
+            if sent == 0:
+                break
+        except BrokenPipeError:
+            break
+        except ConnectionResetError:
+            break
+
+GPIO.output(37,0)
+GPIO.cleanup()
